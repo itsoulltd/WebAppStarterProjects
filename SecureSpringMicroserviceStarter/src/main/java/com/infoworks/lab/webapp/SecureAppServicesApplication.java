164c560e71dea22d45d7ff14f3fd5af7950b7b10
mtowhidislam <m.towhid.islam@gmail.com>
2020-06-23 23:15:14 +0600

    SecureSpringMicroService Starter add with how-to-doc

---
+++ SecureSpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/SecureAppServicesApplication.java
@@ -0,0 +1,25 @@
+package com.infoworks.lab.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.infoworks.lab.controllers"
+        , "com.infoworks.lab.webapp.config"
+        , "com.infoworks.lab.domain"})
+public class SecureAppServicesApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureAppServicesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SecureAppServicesApplication.class);
+    }
+
+}
+
---
+++ SecureSpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.infoworks.lab.webapp.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+            .anyRequest()
+            .requiresSecure();
+    }
+}
