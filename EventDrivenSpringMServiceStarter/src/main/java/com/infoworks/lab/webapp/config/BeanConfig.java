92b0fc12600c3db3290caa670a48c5c2b3b58a03
Towhidul Islam <towhidul.islam@nagad.com.bd>
2020-12-11 14:22:31 +0600

    EventDrivenSpringMService ..refactoring

--- EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
+++ EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/webapp/config/BeanConfig.java
@@ -2,47 +2,26 @@ package com.infoworks.lab.webapp.config;
 
 import com.infoworks.lab.domain.datasources.MemCache;
 import com.infoworks.lab.domain.entities.Passenger;
-import com.infoworks.lab.jsql.ExecutorType;
-import com.infoworks.lab.jsql.JsqlConfig;
-import com.it.soul.lab.sql.SQLExecutor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
 
 @Configuration
 public class BeanConfig {
 
+    private Environment env;
+
+    public BeanConfig(@Autowired Environment env) {
+        this.env = env;
+    }
+
     @Bean("HelloBean")
     public String getHello(){
         return "Hi Spring Hello";
-    }
-
-    @Bean
-    JsqlConfig getJsqlConfig(){
-        return new JsqlConfig();
-    }
-
-    @Autowired
-    private Environment env;
-
-    @Bean("JDBConnectionPool")
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
-    SQLExecutor executor() {
-        SQLExecutor exe = (SQLExecutor) getJsqlConfig().create(ExecutorType.SQL, appDBNameKey());
-        System.out.println("Created DB Connections.");
-        return exe;
-    }
-
-    @Bean("AppDBNameKey")
-    String appDBNameKey(){
-        return env.getProperty("app.db.name");
     }
 
     @Bean
