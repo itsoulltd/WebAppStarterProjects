adaf9a754121622380b86dc0d9d703f1a42fcc85
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-25 10:08:15 +0600

    ExcelItemWriter updated

--- SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/domain/definition/ExcelItemWriter.java
+++ SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/domain/definition/ExcelItemWriter.java
@@ -5,24 +5,31 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.item.ItemWriter;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public interface ExcelItemWriter<T> extends ItemWriter<T>, JobExecutionListener {
 
-    default ExcelParsingService.AsyncWriter createWriter(ExcelParsingService service){
+    default ExcelParsingService.AsyncWriter createWriter(ExcelParsingService service) throws NullPointerException{
+        if (service == null) throw new NullPointerException("ExcelParsingService must not be null!");
         //Create WRITER:
-        ExcelParsingService.AsyncWriter writer = (createAsyncWriter())
+        ExcelParsingService.AsyncWriter writer = createAsyncWriter()
                 ? service.createAsyncWriter(getBatchSize(), getOutputName(), true)
                 : service.createWriter(true, getOutputName(), true);
         return writer;
     }
+    ExcelParsingService getService();
     ExcelParsingService.AsyncWriter getWriter();
     String getOutputName();
     int getBatchSize();
     Logger getLog();
     default boolean createAsyncWriter(){return false;}
+    String[] getColumnHeaders();
+    String getSheetName();
 
     @Override
     default void write(List<? extends T> list) throws Exception{
@@ -37,6 +44,15 @@ public interface ExcelItemWriter<T> extends ItemWriter<T>, JobExecutionListener 
     @Override
     default void beforeJob(JobExecution jobExecution) {
         getLog().info("ExcelItemWriter Started!");
+        //WRITE Column Headers:
+        if (getWriter() != null){
+            if (getColumnHeaders().length > 0){
+                Map<Integer, List<String>> results = new HashMap<>();
+                results.put(0, Arrays.asList(getColumnHeaders()));
+                //Print: headers
+                getWriter().write(getSheetName(), results, false);
+            }
+        }
     }
 
     @Override
@@ -51,4 +67,5 @@ public interface ExcelItemWriter<T> extends ItemWriter<T>, JobExecutionListener 
             }
         }
     }
+
 }
