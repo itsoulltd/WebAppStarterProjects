8652c84ab59062db6e134aba0f874392ff8e72c3
Towhidul Islam <m.towhid.islam@gmail.com>
2021-09-26 11:22:32 +0600

    refactoring .. continue...

--- SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/domain/definition/ExcelItemWriter.java
+++ SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/domain/definition/ExcelItemWriter.java
@@ -1,12 +1,12 @@
 package com.infoworks.lab.domain.definition;
 
 import com.infoworks.lab.services.definition.ContentWriter;
-import com.infoworks.lab.services.impl.ExcelParsingService;
+import com.infoworks.lab.services.impl.ExcelWritingService;
 
 public interface ExcelItemWriter<T> extends ReportItemWriter<T>{
 
-    default ContentWriter createWriter(ExcelParsingService service) throws NullPointerException {
-        if (service == null) throw new NullPointerException("ExcelParsingService must not be null!");
+    default ContentWriter createWriter(ExcelWritingService service) throws NullPointerException {
+        if (service == null) throw new NullPointerException("ExcelWritingService must not be null!");
         //Create WRITER:
         ContentWriter writer = createAsyncWriter()
                 ? service.createAsyncWriter(getBatchSize(), getOutputName(), true)
@@ -15,7 +15,7 @@ public interface ExcelItemWriter<T> extends ReportItemWriter<T>{
     }
 
     int getBatchSize();
-    ExcelParsingService getService();
+    ExcelWritingService getService();
     default boolean createAsyncWriter(){return false;}
 
 }
