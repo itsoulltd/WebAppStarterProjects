d6ac32f2a6790f9be78b14a30680787a472d3ca3
Towhid Islam <m.towhid.islam@gmail.com>
2021-11-13 12:44:21 +0600

    updated

--- SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("com.lab.infoworks"))
+                .apis(RequestHandlerSelectors.basePackage("com.infoworks.lab"))
                 .build();
     }
 }
