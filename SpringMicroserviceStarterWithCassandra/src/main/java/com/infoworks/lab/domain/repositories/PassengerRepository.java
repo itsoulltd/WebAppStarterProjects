6d2ac4d22c5c3afb87688130f908c1e76f85f7b0
Towhid Islam <m.towhid.islam@gmail.com>
2021-12-26 23:12:34 +0600

    Generic implementation of CqlRepositoty<Entity-Type, Id-Type>

---
+++ SpringMicroserviceStarterWithCassandra/src/main/java/com/infoworks/lab/domain/repositories/PassengerRepository.java
@@ -0,0 +1,30 @@
+package com.infoworks.lab.domain.repositories;
+
+import com.infoworks.lab.domain.entities.Passenger;
+import com.it.soul.lab.cql.CQLExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PassengerRepository implements CqlRepository<Passenger, String>{
+
+    private CQLExecutor executor;
+
+    public PassengerRepository(CQLExecutor executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public CQLExecutor getExecutor() {
+        return executor;
+    }
+
+    @Override
+    public String getPrimaryKeyName() {
+        return "uuid";
+    }
+
+    @Override
+    public Class<Passenger> getEntityType() {
+        return Passenger.class;
+    }
+}
