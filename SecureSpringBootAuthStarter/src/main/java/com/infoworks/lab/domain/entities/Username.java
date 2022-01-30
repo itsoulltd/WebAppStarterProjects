1dbd262c42d73ce29b13ff8cbffeb95f08a3bf80
Towhidul Islam <towhidul.islam@nagad.com.bd>
2022-01-30 20:27:52 +0600

    Refactoring: enabling Jpa-Auditing into auth starter

--- SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/domain/entities/Username.java
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/domain/entities/Username.java
@@ -4,11 +4,19 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Username {
+
     private String username;
 
+    public Username() {}
     public Username(String username) {
         this.username = username;
     }
 
-    public Username() {}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/PrimaryJPAConfig.java
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/PrimaryJPAConfig.java
@@ -1,5 +1,6 @@
 package com.infoworks.lab.webapp.config;
 
+import com.infoworks.lab.domain.entities.Username;
 import com.infoworks.lab.jsql.DataSourceKey;
 import com.infoworks.lab.jsql.JsqlConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Configuration
+@EnableJpaAuditing
 @EnableTransactionManagement
 @EnableJpaRepositories(
         basePackages = {"com.infoworks.lab.domain.repositories"}
@@ -66,7 +75,6 @@ public class PrimaryJPAConfig {
     @Primary
     @Bean
     public DataSource dataSource(){
-        //
         DataSource dataSource = DataSourceBuilder
                 .create()
                 .username(username)
@@ -82,7 +90,6 @@ public class PrimaryJPAConfig {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             EntityManagerFactoryBuilder builder
             , DataSource dataSource){
-        //
         return builder
                 .dataSource(dataSource)
                 .packages("com.infoworks.lab.domain.entities")
@@ -94,8 +101,17 @@ public class PrimaryJPAConfig {
     @Bean
     public PlatformTransactionManager transactionManager(
             EntityManagerFactory entityManagerFactory){
-        //
         return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Bean
+    public AuditorAware<Username> auditor() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(UserDetails.class::cast)
+                .map(u -> new Username(u.getUsername()));
     }
 
 }
