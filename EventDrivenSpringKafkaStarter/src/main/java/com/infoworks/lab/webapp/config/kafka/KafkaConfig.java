abc6219b3d68435119dc7e368df865e2600b2c89
Towhid Islam <m.towhid.islam@gmail.com>
2022-03-18 21:40:41 +0600

    kafka:refactor: updating and code organization

--- EventDrivenSpringKafkaStarter/src/main/java/com/infoworks/lab/webapp/config/kafka/KafkaConfig.java
+++ EventDrivenSpringKafkaStarter/src/main/java/com/infoworks/lab/webapp/config/kafka/KafkaConfig.java
@@ -7,7 +7,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.connect.json.JsonSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +20,8 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,16 +34,20 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Bean("kafkaMessageTemplate")
-    public KafkaTemplate<String, Message> getKafkaMessageTemplate() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        //
-        DefaultKafkaProducerFactory factory = new DefaultKafkaProducerFactory<>(props);
-        return new KafkaTemplate<>(factory);
+    @Value("${app.zookeeper.servers}")
+    private String zookeeperServers;
+
+    @Bean("myZooKeeper")
+    public ZkClient getZookeeper() {
+        int sessionTimeout = 10 * 1000;
+        int connectionTimeout = 8 * 1000;
+        return new ZkClient(zookeeperServers
+                , sessionTimeout
+                , connectionTimeout
+                , ZKStringSerializer$.MODULE$);
     }
+
+    /////////////////Kafka-Text-Based-Templates & Consumer-Factory////////////////////////
 
     @Bean("kafkaTextTemplate")
     public KafkaTemplate<String, String> getKafkaTextTemplate() {
@@ -58,7 +63,7 @@ public class KafkaConfig {
     @Value("${app.consumer.group.id}")
     private String consumerGroupID;
 
-    private ConsumerFactory<String, String> getConsumerFactory() {
+    private ConsumerFactory<String, String> getConsumerTextFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -66,13 +71,15 @@ public class KafkaConfig {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupID);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
+        return new DefaultKafkaConsumerFactory<>(props
+                , new StringDeserializer()
+                , new StringDeserializer());
     }
 
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(getConsumerFactory());
+        factory.setConsumerFactory(getConsumerTextFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         //based on partition config:
         //factory.setConcurrency(5);
@@ -82,17 +89,45 @@ public class KafkaConfig {
         return factory;
     }
 
-    @Value("${app.zookeeper.servers}")
-    private String zookeeperServers;
+    /////////////////Kafka-Message-Based-Templates & Consumer-Factory////////////////////////
 
-    @Bean("myZooKeeper")
-    public ZkClient getZookeeper() {
-        int sessionTimeout = 10 * 1000;
-        int connectionTimeout = 8 * 1000;
-        return new ZkClient(zookeeperServers
-                , sessionTimeout
-                , connectionTimeout
-                , ZKStringSerializer$.MODULE$);
+    @Bean("kafkaMessageTemplate")
+    public KafkaTemplate<String, Message> getKafkaMessageTemplate() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        //
+        DefaultKafkaProducerFactory factory = new DefaultKafkaProducerFactory<>(props);
+        return new KafkaTemplate<>(factory);
+    }
+
+    private ConsumerFactory<String, Message> getConsumerMessageFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupID);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new DefaultKafkaConsumerFactory<>(props
+                , new StringDeserializer()
+                , new JsonDeserializer<>(Message.class));
+    }
+
+    //Add this to @KafkaListener(topics = {"${topic.xyz}"}, containerFactory = "messageListenerContainerFactory")
+    //when using with consumerListener.
+    @Bean("messageListenerContainerFactory")
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Message>> kafkaMessageListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, Message> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(getConsumerMessageFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        //based on partition config:
+        //factory.setConcurrency(5);
+        //filtering example
+        //In this listener, all the messages matching the filter will be discarded.
+        //factory.setRecordFilterStrategy(consumerRecord -> consumerRecord.value().contains("loc:"));
+        return factory;
     }
 
 }
