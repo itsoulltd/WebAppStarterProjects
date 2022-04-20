622ce2235280b21e0a34d36f494fab8e10bf50e4
Towhid Islam <m.towhid.islam@gmail.com>
2022-04-20 19:20:57 +0600

    kafka:update

--- EventDrivenSpringKafkaStarter/src/main/java/com/infoworks/lab/domain/beans/queue/TaskQueueManager.java
+++ EventDrivenSpringKafkaStarter/src/main/java/com/infoworks/lab/domain/beans/queue/TaskQueueManager.java
@@ -1,26 +1,32 @@
 package com.infoworks.lab.domain.beans.queue;
 
+import com.infoworks.lab.beans.queue.AbstractTaskQueueManager;
 import com.infoworks.lab.beans.tasks.definition.QueuedTaskLifecycleListener;
-import com.infoworks.lab.beans.queue.*;
+import com.infoworks.lab.beans.tasks.definition.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Component
-@PropertySource("classpath:application.properties")
-@PropertySource("classpath:kafka.properties")
 public class TaskQueueManager extends AbstractTaskQueueManager {
 
     private static final Logger logger = Logger.getLogger("TaskQueueManager");
 
     public TaskQueueManager(@Autowired QueuedTaskLifecycleListener listener) {
         super(listener);
+    }
+
+    @Override
+    protected Task createTask(String text) throws ClassNotFoundException, IOException, IllegalAccessException, InstantiationException {
+        Task task = super.createTask(text);
+        //Inject dependency into Task during MOM's task execution.
+        return task;
     }
 
     @KafkaListener(topics = {"${topic.execute}"}, concurrency = "5")
