53e273ed493704cafbf8d805eec8434ef0a57d14
Towhidul Islam <m.towhid.islam@gmail.com>
2022-08-02 10:37:01 +0600

    batch: updated

--- SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/batch/SimpleBatchScheduler.java
+++ SpringBatchProcessingStarter/src/main/java/com/infoworks/lab/batch/SimpleBatchScheduler.java
@@ -29,19 +29,6 @@ public class SimpleBatchScheduler {
     @Value("${batch.processing.cron.prevent.execution}")
     private boolean shouldPreventExecution;
 
-    /**
-     *  (second, minute, hour, day of month, month, day(s) of week)
-     *  (* * * * * *)
-     *
-     *  "0 0 * * * *" = the top of every hour of every day.
-     *  "*\/10 * * * * *" = every ten seconds.
-     *  "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
-     *  "0 0 8,10 * * *" = 8 and 10 o'clock of every day.
-     *  "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
-     *  "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
-     *  "0 0 0 25 12 ?" = every Christmas Day at midnight
-     */
-
     @Async
     @Scheduled(cron = "${batch.processing.cron.expression}")
     public void process() {
