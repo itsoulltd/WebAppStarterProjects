10d443aeecc9f408360c78904c9230b559275208
Towhidul Islam <m.towhid.islam@gmail.com>
2022-08-19 17:22:38 +0600

    auth: updated

--- SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/domain/repositories/UserRepository.java
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/domain/repositories/UserRepository.java
@@ -2,11 +2,11 @@ package com.infoworks.lab.domain.repositories;
 
 import com.infoworks.lab.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
-
 }
