15b193491ab34629cf0c17b7c94084418d3ffd82
Towhidul Islam <m.towhid.islam@gmail.com>
2022-11-10 11:48:03 +0600

    mongo:update:continue...

--- SpringMicroserviceStarterWithMongo/src/main/java/com/infoworks/lab/services/iDocumentService.java
+++ SpringMicroserviceStarterWithMongo/src/main/java/com/infoworks/lab/services/iDocumentService.java
@@ -3,15 +3,25 @@ package com.infoworks.lab.services;
 import com.infoworks.lab.rest.models.SearchQuery;
 import com.it.soul.lab.data.base.DataSource;
 
-import java.util.*;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public interface iDocumentService<File> extends DataSource<String, File> {
     File findByName(String name);
+    /**
+     * TO Search files (containing file-name), pass SearchQuery.add("filename").isEqualTo("abc");
+     * where filename should be the key.
+     * TO Search files in Directory (containing directory-name), pass SearchQuery.add("dirname").isEqualTo("abc");
+     * where dirname should be the key.
+     * @param query
+     * @return List<InputStream>
+     */
     List<File> search(SearchQuery query);
     default void search(SearchQuery query, BiConsumer<Long, List<File>> consumer) {
-        if (consumer != null) consumer.accept(0l, new ArrayList<>());
+        if (consumer != null) consumer.accept(0l, search(query));
     }
     default Map<Long, List<File>> searchInBatchGroup(SearchQuery query) {return new HashMap<>();}
     default long remove(SearchQuery query) {return 0l;}
