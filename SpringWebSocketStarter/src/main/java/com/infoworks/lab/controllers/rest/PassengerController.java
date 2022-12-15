4b55709c6b72dab72c672cb00edc8323cf70a214
Towhidul Islam <m.towhid.islam@gmail.com>
2022-12-15 14:02:14 +0600

    socket: PassengerController impl RestRepository interface

--- SpringWebSocketStarter/src/main/java/com/infoworks/lab/controllers/rest/PassengerController.java
+++ SpringWebSocketStarter/src/main/java/com/infoworks/lab/controllers/rest/PassengerController.java
@@ -2,9 +2,11 @@ package com.infoworks.lab.controllers.rest;
 
 import com.infoworks.lab.domain.entities.Passenger;
 import com.infoworks.lab.rest.models.ItemCount;
+import com.infoworks.lab.rest.repository.RestRepository;
 import com.it.soul.lab.data.simple.SimpleDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import java.util.Arrays;
@@ -12,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
-public class PassengerController {
+public class PassengerController implements RestRepository<Passenger, String> {
 
     private SimpleDataSource<String, Passenger> dataSource;
 
@@ -22,44 +24,53 @@ public class PassengerController {
     }
 
     @GetMapping("/rowCount")
-    public ItemCount getRowCount(){
+    public ItemCount rowCount(){
         ItemCount count = new ItemCount();
         count.setCount(Integer.valueOf(dataSource.size()).longValue());
         return count;
     }
 
     @GetMapping
-    public List<Passenger> query(@RequestParam("limit") Integer limit
-            , @RequestParam("offset") Integer offset){
-        //TODO: Test with RestExecutor
-        List<Passenger> passengers = Arrays.asList(dataSource.readSync(offset, limit));
+    public List<Passenger> fetch(
+            @RequestParam(value = "limit", defaultValue = "10", required = false) Integer limit
+            , @RequestParam(value = "page", defaultValue = "0", required = false) Integer page){
+        //
+        if (limit < 0) limit = 10;
+        if (page < 0) page = 0;
+        List<Passenger> passengers = Arrays.asList(dataSource.readSync(page, limit));
         return passengers;
     }
 
-    @PostMapping @SuppressWarnings("Duplicates")
-    public ItemCount insert(@Valid @RequestBody Passenger passenger){
-        //TODO: Test with RestExecutor
+    @PostMapping
+    public Passenger insert(@Valid @RequestBody Passenger passenger){
+        //
         dataSource.put(passenger.getName(), passenger);
-        ItemCount count = new ItemCount();
-        count.setCount(Integer.valueOf(dataSource.size()).longValue());
-        return count;
+        return passenger;
     }
 
-    @PutMapping @SuppressWarnings("Duplicates")
-    public ItemCount update(@Valid @RequestBody Passenger passenger){
-        //TODO: Test with RestExecutor
-        Passenger old = dataSource.replace(passenger.getName(), passenger);
-        ItemCount count = new ItemCount();
-        if (old != null)
-            count.setCount(Integer.valueOf(dataSource.size()).longValue());
-        return count;
+    @PutMapping
+    public Passenger update(@Valid @RequestBody Passenger passenger
+            , @ApiIgnore @RequestParam(value = "name", required = false) String name){
+        //
+        dataSource.replace(passenger.getName(), passenger);
+        return passenger;
     }
 
     @DeleteMapping
-    public Boolean delete(@RequestParam("name") String name){
-        //TODO: Test with RestExecutor
+    public boolean delete(@RequestParam("name") String name){
+        //
         Passenger deleted = dataSource.remove(name);
         return deleted != null;
+    }
+
+    @Override
+    public String getPrimaryKeyName() {
+        return "id";
+    }
+
+    @Override
+    public Class<Passenger> getEntityType() {
+        return Passenger.class;
     }
 
 }
