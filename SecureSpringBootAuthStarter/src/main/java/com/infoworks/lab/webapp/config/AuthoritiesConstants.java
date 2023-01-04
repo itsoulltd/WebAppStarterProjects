aef292601f409482836c208e271afa792ff4d16d
Towhid Islam <m.towhid.islam@gmail.com>
2023-01-04 19:15:00 +0600

    auth-service:feat: adding some security-utils from JHipster code base.

---
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/AuthoritiesConstants.java
@@ -0,0 +1,26 @@
+package com.infoworks.lab.webapp.config;
+
+/**
+ * Constants for Spring Security authorities.
+ * JHipster implementation
+ */
+public final class AuthoritiesConstants {
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String CUSTOMER = "ROLE_CUSTOMER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    public static final String DRIVER = "ROLE_DRIVER";
+
+    public static final String OFFICER = "ROLE_OFFICER";
+
+    public static final String LEADER = "ROLE_LEADER";
+
+    public static final String GUEST = "ROLE_GUEST";
+
+    private AuthoritiesConstants() {}
+}
---
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/SecurityUtils.java
@@ -0,0 +1,96 @@
+package com.infoworks.lab.webapp.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+/**
+ * Utility class for Spring Security.
+ * JHipster implementation
+ */
+public final class SecurityUtils {
+
+    private SecurityUtils() {}
+
+    /**
+     * Get the login of the current user.
+     *
+     * @return the login of the current user
+     */
+    public static Optional<String> getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .map(authentication -> {
+                    if (authentication.getPrincipal() instanceof UserDetails) {
+                        UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                        return springSecurityUser.getUsername();
+                    } else if (authentication.getPrincipal() instanceof String) {
+                        return (String) authentication.getPrincipal();
+                    }
+                    return null;
+                });
+    }
+
+    /**
+     * Get the JWT of the current user.
+     *
+     * @return the JWT of the current user
+     */
+    public static Optional<String> getCurrentUserJwt() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .filter(authentication -> authentication.getCredentials() instanceof String)
+                .map(authentication -> (String) authentication.getCredentials());
+    }
+
+    public static String getGuestId() {
+        return SecurityUtils.getCurrentUserLogin()
+                .orElseThrow(() -> new RuntimeException("Guest Id not found!"));
+    }
+
+    /**
+     * Check if a user is authenticated.
+     *
+     * @return true if the user is authenticated, false otherwise
+     */
+    public static boolean isAuthenticated() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .map(authentication -> authentication.getAuthorities().stream()
+                        .noneMatch(grantedAuthority -> grantedAuthority.getAuthority()
+                                .equals(AuthoritiesConstants.ANONYMOUS)))
+                .orElse(false);
+    }
+
+    /**
+     * If the current user has a specific authority (security role).
+     * <p>
+     * The name of this method comes from the isUserInRole() method in the Servlet API
+     *
+     * @param authorities the authority to check
+     * @return true if the current user has the authority, false otherwise
+     */
+    public static boolean isCurrentUserInRole(String...authorities) {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Set<String> authoritiesSet = new HashSet<>(Arrays.asList(authorities));
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .map(authentication -> authentication.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .anyMatch(authoritiesSet::contains))
+                .orElse(false);
+    }
+
+    public static boolean matchAnyAdminRole(Collection<? extends GrantedAuthority> authority) {
+        String[] args = AuthorityUtils.authorityListToSet(authority).toArray(new String[0]);
+        return matchAnyAdminRole(args);
+    }
+
+    public static boolean matchAnyAdminRole(String...args) {
+        return String.join(" ", args).toUpperCase().contains("ADMIN");
+    }
+
+}
