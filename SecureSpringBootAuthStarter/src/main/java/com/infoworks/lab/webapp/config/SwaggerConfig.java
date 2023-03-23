9556072afba69c225490ac7e838ca444ca8cc2e2
Towhid Islam <m.towhid.islam@gmail.com>
2023-03-23 13:39:30 +0100

    secure:doc: updating swagger-2 config

--- SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
+++ SecureSpringBootAuthStarter/src/main/java/com/infoworks/lab/webapp/config/SwaggerConfig.java
@@ -5,18 +5,58 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.infoworks.lab"))
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "1.0",
+                "https://lab.infoworks.com/termsOfAggrements",
+                new Contact("InfoWorks Lab Ltd", "https://lab.infoworks.com", "contact@infoworks.com"),
+                "License of API",
+                "https://lab.infoworks.com/license",
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        return Arrays.asList(
+                new SecurityReference("JWT", new AuthorizationScope[]{authorizationScope})
+        );
     }
 }
