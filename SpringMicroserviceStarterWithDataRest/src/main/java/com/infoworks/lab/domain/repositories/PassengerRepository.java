033727cda4ba5b3f4aaf1a6f0f032911119af90a
Towhid Islam <m.towhid.islam@gmail.com>
2023-12-10 00:04:31 +0100

    data-rest:chore: updated springfox-swagger dependency

--- SpringMicroserviceStarterWithDataRest/src/main/java/com/infoworks/lab/domain/repositories/PassengerRepository.java
+++ SpringMicroserviceStarterWithDataRest/src/main/java/com/infoworks/lab/domain/repositories/PassengerRepository.java
@@ -3,7 +3,6 @@ package com.infoworks.lab.domain.repositories;
 import com.infoworks.lab.domain.entities.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
