c708bef6a460e6894e30826fc93b6c972349a62c
Towhid Islam <m.towhid.islam@gmail.com>
2024-05-06 12:48:27 +0200

    web-socket:fix: security interceptors.

--- SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/config/socket/interceptor/ByPassAuthorizationInterceptor.java
+++ SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/config/socket/interceptor/ByPassAuthorizationInterceptor.java
@@ -1,9 +1,15 @@
 package com.infoworks.lab.webapp.config.socket.interceptor;
 
+import com.infoworks.lab.rest.models.Message;
 import com.infoworks.lab.webapp.config.socket.session.SessionAttributeKey;
+import com.infoworks.lab.webapp.config.socket.session.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.socket.WebSocketHandler;
 
 import javax.servlet.http.HttpSession;
@@ -11,18 +17,40 @@ import java.util.Map;
 
 public class ByPassAuthorizationInterceptor extends WSocketAuthorizationInterceptor {
 
+    private static Logger LOG = LoggerFactory.getLogger(ByPassAuthorizationInterceptor.class.getSimpleName());
+
     public ByPassAuthorizationInterceptor() {}
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Map<String, Object> attributes) throws Exception {
-        //super.beforeHandshake(request, response, webSocketHandler, attributes);
+        LOG.info("beforeHandshake: Called");
+        ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
         //Testing HttpSession:
         HttpSession session = ((ServletServerHttpRequest) request).getServletRequest().getSession();
         if (session != null) {
             attributes.put(SessionAttributeKey.SessionID.name(), session.getId());
-            LOG.info("HttpSession ID: " + session.getId());
         }
         //By-Passing Security:
+        MultiValueMap<String, String> parameters = getQueryParams(servletRequest);
+        LOG.info(Message.marshal(parameters));
+        parameters.forEach((key, value) -> {
+            if (key.trim().equalsIgnoreCase(HttpHeaders.AUTHORIZATION))
+                attributes.put(SessionAttributeKey.Token.name(), parameters.getFirst(key));
+            else if (key.trim().equalsIgnoreCase(SessionAttributeKey.UserID.name()))
+                attributes.put(SessionAttributeKey.UserID.name(), parameters.getFirst(key));
+            else if (key.trim().equalsIgnoreCase(SessionAttributeKey.TenantID.name()))
+                attributes.put(SessionAttributeKey.TenantID.name(), parameters.getFirst(key));
+            else
+                attributes.put(key, parameters.getFirst(key));
+        });
+        //Create-User-Session:
+        LOG.info(Message.marshal(attributes));
+        UserSession.create(attributes);
         return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+        LOG.info("afterHandshake: Called");
     }
 }
--- SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/config/socket/interceptor/WSocketAuthorizationInterceptor.java
+++ SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/config/socket/interceptor/WSocketAuthorizationInterceptor.java
@@ -1,8 +1,10 @@
 package com.infoworks.lab.webapp.config.socket.interceptor;
 
 import com.infoworks.lab.jjwt.JWTValidator;
+import com.infoworks.lab.rest.models.Message;
 import com.infoworks.lab.webapp.config.JWTokenValidator;
 import com.infoworks.lab.webapp.config.socket.session.SessionAttributeKey;
+import com.infoworks.lab.webapp.config.socket.session.UserSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -20,62 +22,62 @@ import java.util.Map;
 
 public class WSocketAuthorizationInterceptor implements HandshakeInterceptor {
 
-    protected static Logger LOG = LoggerFactory.getLogger(WSocketAuthorizationInterceptor.class.getSimpleName());
-
-    @Override
-    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
-        //TODO:
-    }
+    private static Logger LOG = LoggerFactory.getLogger(WSocketAuthorizationInterceptor.class.getSimpleName());
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Map<String, Object> attributes) throws Exception {
+        LOG.info("beforeHandshake: Called");
         if (request instanceof ServletServerHttpRequest) {
-            //
-            ServletServerHttpRequest servletRequest
-                    = (ServletServerHttpRequest) request;
-
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
             //Security Check here:
-            MultiValueMap<String, String> parameters = getAuthParameters(servletRequest);
+            MultiValueMap<String, String> parameters = getQueryParams(servletRequest);
+            LOG.info(Message.marshal(parameters));
             //
             if (parameters.containsKey(HttpHeaders.AUTHORIZATION)){
-
-                //Fetch Tenant's Secret and Validate the JWT Token.
-                String token = parameters.getFirst(HttpHeaders.AUTHORIZATION);
-                attributes.put(SessionAttributeKey.Token.name(), token);
-                //TODO: Map additional header parameter from client:
-                //
                 HttpSession session = servletRequest.getServletRequest().getSession();
                 if (session != null) {
                     attributes.put(SessionAttributeKey.SessionID.name(), session.getId());
-                    LOG.info("HttpSession ID: " + session.getId());
                 }
+                //TODO: Map additional header parameter from client:
+                parameters.forEach((key, value) -> {
+                    if (key.trim().equalsIgnoreCase(SessionAttributeKey.UserID.name()))
+                        attributes.put(SessionAttributeKey.UserID.name(), parameters.getFirst(key));
+                    else if (key.trim().equalsIgnoreCase(SessionAttributeKey.TenantID.name()))
+                        attributes.put(SessionAttributeKey.TenantID.name(), parameters.getFirst(key));
+                    else
+                        attributes.put(key, parameters.getFirst(key));
+                });
+                //Fetch Tenant's Secret and Validate the JWT Token.
+                String token = parameters.getFirst(HttpHeaders.AUTHORIZATION);
+                attributes.put(SessionAttributeKey.Token.name(), token);
                 //
                 JWTValidator validator = new JWTokenValidator(servletRequest.getServletRequest());
                 boolean isValid = validator.isValid(token);
+                if (isValid) {
+                    LOG.info(Message.marshal(attributes));
+                    UserSession.create(attributes);
+                }
                 return isValid;
-                //
             }
         }
         return false;
     }
 
-    protected MultiValueMap<String, String> getAuthParameters(ServletServerHttpRequest servletRequest) {
+    @Override
+    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+        LOG.info("afterHandshake: Called");
+    }
 
+    protected MultiValueMap<String, String> getQueryParams(ServletServerHttpRequest servletRequest) {
         LOG.info(servletRequest.getURI().toString());
-
         MultiValueMap<String, String> parameters =
                 UriComponentsBuilder.fromUriString(servletRequest.getURI().toString()).build().getQueryParams();
-        if (parameters.isEmpty()){
-            parameters = servletRequest.getHeaders();
-        }
         return parameters;
     }
 
-    protected Map<String, String> getAuthHeaderParameters(ServletServerHttpRequest servletRequest){
-
+    protected Map<String, String> getRequestHeaders(ServletServerHttpRequest servletRequest){
         Map<String, String> map = new HashMap<>();
-
-        MultiValueMap<String, String> headers = getAuthParameters(servletRequest);
+        MultiValueMap<String, String> headers = servletRequest.getHeaders();
         headers.forEach((key, items) ->{
             if (items.size() > 0){
                 if (key.trim().equalsIgnoreCase(HttpHeaders.AUTHORIZATION)){
@@ -84,7 +86,6 @@ public class WSocketAuthorizationInterceptor implements HandshakeInterceptor {
                 }
             }
         });
-
         return map;
     }
 }
