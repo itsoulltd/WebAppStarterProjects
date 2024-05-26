1259014f2e6eab7f7e0e2d7c099a6da89f6f80d4
Towhid Islam <m.towhid.islam@gmail.com>
2024-05-26 15:18:02 +0200

    mservice:feat: adding example of how to do dynamic search in micro-service with simple jdbc-wrapper per request.

--- SpringMicroserviceStarter/src/main/java/com/infoworks/lab/controllers/rest/PassengerController.java
+++ SpringMicroserviceStarter/src/main/java/com/infoworks/lab/controllers/rest/PassengerController.java
@@ -2,20 +2,32 @@ package com.infoworks.lab.controllers.rest;
 
 import com.infoworks.lab.domain.entities.Passenger;
 import com.infoworks.lab.rest.models.ItemCount;
+import com.infoworks.lab.rest.models.SearchQuery;
 import com.infoworks.lab.rest.repository.RestRepository;
 import com.it.soul.lab.data.base.DataSource;
+import com.it.soul.lab.sql.QueryExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
 public class PassengerController implements RestRepository<Passenger, String> {
+
+    /**
+     * Example of inject @Scope beans.
+     * e.g. @RequestScope bean SQLExecutor to do JDBC-Calls to database.
+     */
+    @Resource(name = "executor")
+    private QueryExecutor executor;
 
     private DataSource<String, Passenger> dataSource;
 
@@ -72,6 +84,22 @@ public class PassengerController implements RestRepository<Passenger, String> {
     @Override
     public Class<Passenger> getEntityType() {
         return Passenger.class;
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<List<Passenger>> search(@RequestBody SearchQuery query) throws Exception {
+        //
+        int limit = query.getSize();
+        if (limit <= 0) limit = 10;
+        List<Passenger> passengers = Passenger.read(Passenger.class
+                , executor
+                , query.getPredicate());
+        //
+        limit = passengers.size() > limit ? limit : passengers.size();
+        passengers = (passengers != null && passengers.size() > 0)
+                ? passengers.subList(0, limit)
+                : new ArrayList<>();
+        return ResponseEntity.ok(passengers);
     }
 
 }
