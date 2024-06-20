16fa8b5ee39cde52e1cbb3acc758b7a374834aad
Towhid Islam <m.towhid.islam@gmail.com>
2024-06-20 12:32:08 +0200

    activemq:chore: removing MySQL/Any-RDBMS from project classpath. This kind of example only relay on in-memory redis database and MOM (ActiveMQ/RebbitMQ)

--- EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/domain/entities/Passenger.java
+++ EventDrivenSpringMServiceStarter/src/main/java/com/infoworks/lab/domain/entities/Passenger.java
@@ -1,28 +1,22 @@
 package com.infoworks.lab.domain.entities;
 
 import com.infoworks.lab.domain.validation.constraint.Gender.IsValidGender;
-import com.it.soul.lab.sql.SQLExecutor;
+import com.it.soul.lab.sql.entity.Entity;
 import com.it.soul.lab.sql.entity.Ignore;
 import com.it.soul.lab.sql.entity.PrimaryKey;
 import com.it.soul.lab.sql.entity.TableName;
-import com.it.soul.lab.sql.query.models.Property;
 
-import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Objects;
 
-@Entity(name = "Passenger")
 @TableName(value = "Passenger")
-public class Passenger extends com.it.soul.lab.sql.entity.Entity {
+public class Passenger extends Entity {
 
 	@PrimaryKey(name="id", auto=true)
-	@Id
-	@Column(length = 100)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id = 0;
+	private Integer id;
 
 	@NotNull(message = "name must not be null.")
 	private String name;
@@ -119,11 +113,6 @@ public class Passenger extends com.it.soul.lab.sql.entity.Entity {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
-	}
-
-	public Property getPropertyTest(String key, SQLExecutor exe, boolean skipPrimary) {
-		return getProperty(key, exe, skipPrimary);
-
 	}
 
 }
