60ce8c697c7cdc100d57a4ce8769f07555554ef3
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-01 00:07:48 +0200

    socket: refactoring filters.

--- SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/filters/CORSFilter.java
+++ SpringWebSocketStarter/src/main/java/com/infoworks/lab/webapp/filters/CORSFilter.java
@@ -1,4 +1,4 @@
-package com.infoworks.lab.webapp.config;
+package com.infoworks.lab.webapp.filters;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.GenericFilterBean;
