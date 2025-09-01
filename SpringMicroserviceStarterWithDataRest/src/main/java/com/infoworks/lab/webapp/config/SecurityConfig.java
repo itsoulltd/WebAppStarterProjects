4e989b146c3afbe6aae553ac306e381d4d9ff91b
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-02 01:43:38 +0200

    all: updated

--- SpringMicroserviceStarterWithDataRest/src/main/java/com/infoworks/lab/webapp/config/SecurityConfig.java
+++ SpringMicroserviceStarterWithDataRest/src/main/java/com/infoworks/lab/webapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.infoworks.lab.webapp.filters.ByPassAuthorizationFilter;
 import com.it.soul.lab.connect.DriverClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
