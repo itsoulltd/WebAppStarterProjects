0fd12169560364bf1ceacb93913e6d752175c442
DEGRUYTER\towhid.islam <towhid.islam@degruyter.com>
2025-09-02 01:39:34 +0200

    mservice:refactor: upgrade to springboot-2.5.15

--- SpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/config/SecurityConfig.java
+++ SpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/config/SecurityConfig.java
@@ -5,19 +5,20 @@ import com.infoworks.lab.webapp.filters.ByPassAuthorizationFilter;
 import com.it.soul.lab.connect.DriverClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Value("${app.disable.security}")
     private boolean disableSecurity;
@@ -41,16 +42,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${spring.datasource.driver-class-name}") String activeDriverClass;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable()
-                //.requiresChannel().anyRequest().requiresSecure() //enable for Https
-                //.and()
-                //.authorizeRequests().anyRequest().authenticated() //enable to restrict all
-                .authorizeRequests().antMatchers("/**").permitAll() //enable to open all
+                .authorizeRequests().antMatchers(URL_WHITELIST).permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated() //enable to restrict all
+                //.authorizeRequests().antMatchers("/**").permitAll() //enable to open all
                 .and()
                 .addFilterBefore(
                         (disableSecurity ? new ByPassAuthorizationFilter() : new AuthorizationFilter())
@@ -59,11 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         if (activeDriverClass.equalsIgnoreCase(DriverClass.H2_EMBEDDED.toString())){
             http.headers().frameOptions().disable();
         }
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(URL_WHITELIST);
+        return http.build();
     }
 
 }
