99f3653a82b7a36ac80c970c831378806acbd550
Towhid Islam <m.towhid.islam@gmail.com>
2026-08-16 11:01:02 +0200

    mservice:fix: updated StartupConfig

--- SpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
+++ SpringMicroserviceStarter/src/main/java/com/infoworks/lab/webapp/config/StartupConfig.java
@@ -1,5 +1,6 @@
 package com.infoworks.lab.webapp.config;
 
+import com.infoworks.connect.JDBCDriverClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.event.ContextClosedEvent;
@@ -10,19 +11,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupConfig implements CommandLineRunner {
 
-    @Value("${server.app.domain}")
-    private String serverDomain;
+    private final String serverDomain;
+    private final String serverPort;
+    private final String servletContext;
+    private final String activeDriverClass;
+    private final String isH2ConsoleEnabled;
+    private final String h2ConsolePath;
 
-    @Value("${server.port}")
-    private String serverPort;
-
-    @Value("${server.servlet.context-path}")
-    private String servletContext;
+    public StartupConfig(@Value("${server.app.domain}") String serverDomain
+            , @Value("${server.port}") String serverPort
+            , @Value("${server.servlet.context-path}") String servletContext
+            , @Value("${spring.datasource.driver-class-name}") String activeDriverClass
+            , @Value("${spring.h2.console.enabled}") String isH2ConsoleEnabled
+            , @Value("${spring.h2.console.path}") String h2ConsolePath) {
+        this.serverDomain = serverDomain;
+        this.serverPort = serverPort;
+        this.servletContext = servletContext;
+        this.activeDriverClass = activeDriverClass;
+        this.isH2ConsoleEnabled = isH2ConsoleEnabled;
+        this.h2ConsolePath = h2ConsolePath;
+    }
 
     @Override
     public void run(String... args) throws Exception {
         //How to use executor:
         System.out.println(String.format("http://%s:%s%s/swagger-ui/index.html", serverDomain, serverPort, servletContext));
+        if (activeDriverClass.equalsIgnoreCase(JDBCDriverClass.H2_EMBEDDED.toString())
+                && Boolean.parseBoolean(isH2ConsoleEnabled)){
+            System.out.println(String.format("http://localhost:%s%s", serverPort, h2ConsolePath));
+        }
         //
     }
 
