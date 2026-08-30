0077547eb3b9f17cf2ebcf2fa5a5517deb62d953
towhid.islam <m.towhid.islam@gmail.com>
2026-08-31 00:17:21 +0200

    http-client:fix: BeanConfig updated.

--- SpringHttpClientStarter/src/main/java/com/infoworks/config/BeanConfig.java
+++ SpringHttpClientStarter/src/main/java/com/infoworks/config/BeanConfig.java
@@ -1,6 +1,8 @@
 package com.infoworks.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.infoworks.objects.MessageParser;
 import com.infoworks.utils.services.iResources;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +18,12 @@ public class BeanConfig {
 
     @Bean
     ObjectMapper getMapper(){
-        return MessageParser.getJsonSerializer();
+        //Solution: Add Jackson JSR-310 Module. Jackson doesn't know how to (de)serialize java.time.LocalDateTime,
+        // because Java 8 time types are not supported out-of-the-box unless you register the JSR-310 module.
+        ObjectMapper mapper = MessageParser.getJsonSerializer();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 
     @Bean
