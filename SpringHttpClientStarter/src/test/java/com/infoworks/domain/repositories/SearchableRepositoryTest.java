ecac4a2d5fecc398632d55ea82d1734420d4892e
towhid.islam <m.towhid.islam@gmail.com>
2026-09-07 13:08:58 +0200

    http-client:test: example of search with pagination.

--- SpringHttpClientStarter/src/test/java/com/infoworks/domain/repositories/SearchableRepositoryTest.java
+++ SpringHttpClientStarter/src/test/java/com/infoworks/domain/repositories/SearchableRepositoryTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @DataJpaTest
 class SearchableRepositoryTest {
@@ -27,5 +29,28 @@ class SearchableRepositoryTest {
         List<User> results = userRepository.search(query, User.class);
         Assertions.assertNotNull(results);
         Assertions.assertEquals(results.size(), 0);
+    }
+
+    public void search(SearchQuery query, SearchableRepository repository, int pageCount, Consumer<List<User>> consumer) {
+        int pageSize = (query.getSize() <= 0) ? 10 : query.getSize();
+        query.setSize(pageSize); //update with validated pageSize.
+        pageCount = (pageCount <= 0) ? 1 : pageCount;
+        int start = 1; //SearchableRepository page-number is 1-based.
+        while (start <= pageCount) {
+            query.setPage(start);
+            List<User> result = repository.search(query, User.class);
+            if (consumer != null) {
+                if (!result.isEmpty()) consumer.accept(result);
+                else { consumer.accept(result); break; }
+            }
+            //Next page:
+            start++;
+        }
+    }
+
+    public long maxPageCount(JpaRepository repository, int pageSize) {
+        long maxCount = repository.count();
+        pageSize = (pageSize <= 0) ? 10 : pageSize;
+        return (pageSize == maxCount) ? 1 : (maxCount / pageSize) + 1;
     }
 }
