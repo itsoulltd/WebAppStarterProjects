96de6b1d91ca48bef0614076ea3df9e045abcfd4
Towhid Islam <m.towhid.islam@gmail.com>
2026-09-13 12:19:13 +0200

    mservice:http-client:test: updated

--- SpringMicroserviceStarter/src/test/java/com/infoworks/lab/services/WebClientTest.java
+++ SpringMicroserviceStarter/src/test/java/com/infoworks/lab/services/WebClientTest.java
@@ -1,29 +1,22 @@
-package com.infoworks.services;
+package com.infoworks.lab.services;
 
-import com.infoworks.config.TestBeanConfig;
+import com.infoworks.lab.webapp.config.BeanConfig;
+import com.infoworks.lab.webapp.config.TestJPAH2Config;
 import com.infoworks.utils.rest.client.DownloadTask;
 import com.infoworks.utils.services.iResources;
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(classes = {TestBeanConfig.class})
+@SpringBootTest(classes = {BeanConfig.class, TestJPAH2Config.class})
 public class WebClientTest {
 
     private static Logger LOG = LoggerFactory.getLogger(WebClientTest.class);
@@ -52,7 +45,7 @@ public class WebClientTest {
         }
     }
 
-    //@Test
+    @Test
     public void downloadTaskTest_Spring() {
         //CAUTION: CHECK DOWNLOAD URL and CONTENT BEFORE RUN THE TEST
         //Test Url-1: https://farm7.staticflickr.com/6089/6115759179_86316c08ff_z_d.jpg
@@ -71,53 +64,6 @@ public class WebClientTest {
                 Assertions.assertNotNull(img);
                 LOG.info("Spring Image Downloaded: " + response.getResource().getFilename());
                 LOG.info("Spring Image Size: " + response.getResource().contentLength());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Autowired
-    private WebClient.Builder wcBuilder;
-
-    @Test
-    public void wcBuilderInjectionTest() {
-        Assertions.assertNotNull(wcBuilder);
-        LOG.info("WebClient.Builder successfully injected!");
-    }
-
-    //@Test
-    public void downloadPdfAndCountWordsTest() throws IOException {
-        //CAUTION: CHECK DOWNLOAD URL and CONTENT BEFORE RUN THE TEST
-        //Test Url-2: https://file-examples.com/storage/fe2b56191b6a91eed93e57a/2017/10/file-sample_150kB.pdf
-
-        WebClient.Builder clientBuilder = (wcBuilder != null) ? wcBuilder : WebClient.builder();
-
-        WebClient webClient =
-                clientBuilder.baseUrl("https://file-examples.com")
-                        .defaultHeaders(headers -> headers.setBasicAuth("username", "password"))
-                        .build();
-        //
-        Resource response = webClient.get()
-                .uri(String.format("/storage/fe2b56191b6a91eed93e57a/2017/10/%s"
-                        , URLEncoder.encode("file-sample_150kB.pdf", Charset.defaultCharset())))
-                .retrieve()
-                .bodyToMono(Resource.class)
-                .block();
-        //
-        if (response.getInputStream() != null) {
-            try (InputStream iso = response.getInputStream()) {
-                PDDocument document = Loader.loadPDF(iso.readAllBytes());
-                assertNotNull(document);
-                //document.close();
-                LOG.info("PDF Downloaded: " + response.getDescription());
-                LOG.info("PDF Size: " + response.contentLength());
-                //
-                WordCounter counter = new WordCounter();
-                long wordCount = counter.pdfWordCount(document);
-                LOG.info("PDF Word Count: " + wordCount);
-                //
-                document.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
